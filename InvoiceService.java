af99c009b9a630188621ebfcbf6646f68f525420
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-28 14:40:49 +0530

    Added: Program To Provide Invoice Service

---
+++ InvoiceService.java
@@ -0,0 +1,11 @@
+package com.cabInvoiceGenerator;
+
+public class InvoiceService {
+    public double distance;
+    public int time;
+
+    public InvoiceService(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
