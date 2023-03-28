ed16d5044b872ade70635ee184b8d836e0db7ea6
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-28 14:29:18 +0530

    Added: Program To Calculate Fare Of Multiple Rides Using Generated Invoice

---
+++ MultipleRidesInvoice.java
@@ -0,0 +1,30 @@
+package com.cabInvoiceGenerator;
+
+import java.util.Scanner;
+public class MultipleRidesInvoice {
+    double distance;
+    double time;
+    private static final double minCostPerKm = 10;
+    private static final double costForTime = 1;
+
+    public double RideOne() {
+        double distance = 15.3;
+        int time = 20;
+        double ride1 = distance * minCostPerKm + time * costForTime;
+        System.out.println(ride1);
+        return ride1;
+    }
+    public double RideTwo(){
+        double distance = 11.0;
+        int time = 15;
+        double ride2 = distance * minCostPerKm + time * costForTime;
+        System.out.println(ride2);
+        return ride2;
+    }
+    public static void main(String[] args) {
+        MultipleRidesInvoice rides = new MultipleRidesInvoice();
+        rides.RideOne();
+        rides.RideTwo();
+    }
+}
+
